db573bc404a6ddc7607a333a9117a2f55808cc92
717243 <piyush.kinge@seagate.com>
2022-08-22 12:01:29 +0530

    Added eslint, prettier, compression, editor config, proxy config, tailwindcss, etc.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    minify: 'esbuild',
+    target: 'esnext',
+  },
+  plugins: [react(), viteCompression()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
